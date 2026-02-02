881b468559eca2d9d7f00d2b35e2dede5284c1e6
vaishnavi256 <yadavvaishnavi95@gmail.com>
2026-02-02 15:02:49 +0530

    fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "https://vaish-wallet.vercel.app", 
   })
 );
 
